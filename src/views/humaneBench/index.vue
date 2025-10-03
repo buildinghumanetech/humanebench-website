27f3bee8ea8fe4c6c5347096b7b5f87d9f1ca0d9
Tseten Changten <ttsetench01@gmail.com>
2025-10-03 20:33:58 +0200

    #14 initiliaze project

---
+++ src/views/humaneBench/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1 class="mt-4 text-center">
+    HumaneBench interactive Dashboard
+  </h1>
+  <p class="text-center">
+    Currently in progress. This is a placeholder.
+  </p>
+</template>
+<script lang="ts">
+</script>
